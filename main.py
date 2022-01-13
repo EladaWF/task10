2b0f4088468fda5d7406ff1cec18f37b1346593c
farmlennost <neket_zhilov@mail.ru>
2022-01-13 14:23:46 +0300

    task10

---
+++ main.py
@@ -0,0 +1,9 @@
+# https://codeforces.com/problemset/problem/1186/A
+input1 = input().split()
+if int(input1[0]) > int(input1[1]) or int(input1[0]) > int(input1[2]):
+    print("NO")
+
+else:
+    print("YES")
+
+
